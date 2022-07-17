7619e31da151d3c6b18d88f6a192e60650302807
Agustin Balart <balartagustin@gmail.com>
2022-07-17 15:27:49 -0300

    Agrego tests

--- js/main.js
+++ js/main.js
@@ -2,45 +2,58 @@
 
 //El archivo .js se invoca desde el html productos
 
-/*********************************E-COMMERCE sobre venta de indumentaria femenina****************************************************************/
+/***************************************E-COMMERCE sobre venta de indumentaria femenina*********************************************************/
 
 
-//Declaro objeto generico a modo de ejemplo
+//Declaro de variables,arrays y objetos
+ 
 
+//Objetos de tipo producto
 const producto = {
-     
-     id:1,
-     nombre:"Remera",
-     precio:1000,
-     stock:9999,
-     descripcion:"Producto generico",
-     cantidadEnCarrito:1
+    id:1,
+    nombre:"Remera",
+    precio:1000,
+    stock:9999,
+    descripcion:"Producto generico",
+    cantidadEnCarrito:1
 }
-      
-
-console.log(producto);
-
-function Producto(Nuevo) {
-    this.id = Nuevo.id;
-    this.nombre = Nuevo.nombre;
-    this.precio = Nuevo.precio;
-    this.stock = Nuevo.stock;
-    this.descripcion = Nuevo.descripcion;
-    this.cantidadEnCarrito = Nuevo.cantidadEnCarrito;
+ 
+const producto1 = {    
+       id:2,
+       nombre:"Jean",
+       precio:500,
+       stock:0,
+       descripcion:"Jeans clasicos",
+       cantidadEnCarrito:0 
     }
 
-const producto1 = new Producto({    
-        id:1,
-        nombre:"Jean",
-        precio:500,
-        stock:0,
-        descripcion:"Jeans clasicos",
-        cantidadEnCarrito:0 });
+const producto2 = {    
+    id:3,
+    nombre:"Ojota",
+    precio:100,
+    stock:1,
+    descripcion:"Ojotas marca Brasil",
+    cantidadEnCarrito:1 
+    }
+    
 
-    console.log(producto);
+let carrito = [producto,producto1]  //Este array almacenara los productos agregados al carrito
+
+console.log('*Los productos en el carrito son: ' + carrito.length)
+imprimirArray(carrito)
 
 //Declaro funciones
 
+
+function imprimirArray(array) {
+ 
+for (const producto of array) {
+console.log(producto.id);
+console.log(producto.nombre);
+}
+}
+
+//Esta funcion valida si un producto cuenta con stock
 function validarStock(producto) {
     const stock = producto.stock;
     console.log("El stock es de:"+ stock)
@@ -49,29 +62,24 @@ function validarStock(producto) {
     }else{
         return 0;
     }
-}
-
-function validarProductoEnCarrito(producto) {
+ }
+ 
+ //Esta funcion valida si un producto se encuentra cargado en el carrito 
+ function validarProductoEnCarrito(producto) {
     if(producto.cantidadEnCarrito > 0){
         return 1;
     }else{
         return 0;
     }
+ }
+ 
+ //Esta funcion obtiene el precio de un producto y lo imprime por consola web
+ function obtenerPrecio(producto) {
+    console.log("El precio del producto es: "+producto.precio)
 }
 
-
-function agregarProductoCarrito(producto) {
-    const tenemosStock = validarStock(producto);
-    if(tenemosStock){
-        console.log('Agregas el producto al carrito: ' + producto.nombre);
-        producto.cantidadEnCarrito++;
-    }else{
-        console.log('Sin stock');
-    }
-    }
-
-
-function eliminarProductoCarrito(producto) {
+ //Elimina producto, version con funcion
+ function eliminarProductoCarrito(producto) {
     const enCarrito = validarProductoEnCarrito(producto);
     if(enCarrito){
         console.log('Eliminando 1 stock de '+producto.nombre+' del carrito');
@@ -81,29 +89,74 @@ function eliminarProductoCarrito(producto) {
         console.log('El producto no está en carrito');
     }
      }
+ 
 
-function obtenerPrecio(producto) {
-         console.log("El precio del producto es: "+producto.precio)
-     }
 
-//Tests
+//Esta funcion elimina un producto del carrito por medio de su id
+function eliminarProducto(producto){
+    if(validarProductoEnCarrito){
+        const productoEliminar = producto.id;
+        const index = carrito.findIndex( x => x.id === productoEliminar );
+        if(index!=-1){
+            console.log('El producto a eliminar es: '+ producto.nombre)
+            carrito.splice(index,1)
+        }
+        else{
+            console.log('El producto no está en el carrito')
+        }
+       
+    }
+    console.log('Ahora la cantidad de productos es de: ' + carrito.length)
+     
+}
 
-console.log("obtenerPrecio");
-obtenerPrecio(producto);  
 
-console.log("agregarProductoCarrito");
-agregarProductoCarrito(producto);
+//Agregar producto al carrito
 
-console.log("eliminarProductoCarrito");
-eliminarProductoCarrito(producto);
+function AgregarProducto()
+{
+        let respuesta = prompt("Desea agregar un nuevo producto al carrito? Presione S para confirmar o N para denegar")
+        switch(respuesta){
+        case 'S': 
+        const productoNuevo = {id:3,nombre:"Sombrero",precio:500,stock:23,descripcion:"Sombrero de mujer",cantidadEnCarrito:4 }
+        carrito.push(productoNuevo)
+        console.log('Los productos en el carrito son:' + carrito.length)
+        console.log('El producto agregado es: '+carrito[carrito.length-1].nombre)
+        alert('Producto agregado')
+        break
+        case 'N':
+        alert('Usted no desa agregar nuevos productos al carrito')
+        break
+        default:
+        alert('Respuesta invalida')
+        break
+        }
+        console.log('Ahora la cantidad de productos es de: ' + carrito.length)
+        
+}
 
-//Producto1
 
-console.log("obtenerPrecio");
-obtenerPrecio(producto1);  
+//Esta funcion verifica la existencia de un producto en el array carrito de compras
 
-console.log("agregarProductoCarrito");
-agregarProductoCarrito(producto1);
+function validarProductoEnCarrito2(producto){
+    console.log(carrito.includes(producto)) 
+    return carrito.includes(producto)
+}
+   
+//Tests sobre array de objetos + simulacion de flujo
+ 
+console.log('****Imprimir array****')
+imprimirArray(carrito)
 
-console.log("eliminarProductoCarrito");
-eliminarProductoCarrito(producto1);
+console.log('****Elimino objeto producto****')
+
+eliminarProducto(producto1) //jean
+imprimirArray(carrito)
+
+console.log('****Valido objeto producto****')
+validarProductoEnCarrito2(producto2) //Ojota
+
+
+console.log('****Agregar objeto producto****')
+AgregarProducto()  //Version con array objeto
+imprimirArray(carrito)
